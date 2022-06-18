bd3cb9535130f05811dbdee3f315e074e5d4056d
SB-9838 <86655602+SB-9838@users.noreply.github.com>
2022-06-18 09:00:07 +0300

    Create GoHomeFromOfflinePractiseMessage.java

---
+++ src/main/java/Messaging/Client/Battle/GoHomeFromOfflinePractiseMessage.java
@@ -0,0 +1,32 @@
+package com.java.brawl.sb.Messaging.Client.Battle;
+
+import java.io.DataOutputStream;
+
+import com.java.brawl.sb.Sessions.PlayerSession;
+
+import com.java.brawl.sb.DataStream.ByteStream;
+import com.java.brawl.sb.Messaging.Server.Home.OwnHomeDataMessage;
+
+public class GoHomeFromOfflinePractiseMessage extends ByteStream {
+
+	private ByteStream stream;
+	private PlayerSession player;
+	private DataOutputStream writer;
+
+	public GoHomeFromOfflinePractiseMessage(PlayerSession plr,  byte[] payload, DataOutputStream Writer) {
+		
+		super(payload);
+		this.writer = Writer;
+		this.player = plr;
+
+	}
+
+	public GoHomeFromOfflinePractiseMessage decode() {
+		return this;
+	}
+
+	public void process() {
+
+		new OwnHomeDataMessage(this.player, this.writer).encode();
+	}
+}
