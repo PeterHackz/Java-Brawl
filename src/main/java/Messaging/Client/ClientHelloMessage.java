dc0673e6f1f02e9f25da96fd2eb46325389aec34
SB-9838 <86655602+SB-9838@users.noreply.github.com>
2022-06-18 08:59:04 +0300

    Create ClientHelloMessage.java

---
+++ src/main/java/Messaging/Client/ClientHelloMessage.java
@@ -0,0 +1,32 @@
+package com.java.brawl.sb.Messaging.Client;
+
+import java.io.DataOutputStream;
+
+import com.java.brawl.sb.Sessions.PlayerSession;
+
+import com.java.brawl.sb.DataStream.ByteStream;
+import com.java.brawl.sb.Messaging.Server.ServerHelloMessage;
+
+public class ClientHelloMessage extends ByteStream {
+	
+	private ByteStream stream;
+	private PlayerSession player;
+	private DataOutputStream writer;
+
+	public ClientHelloMessage(PlayerSession plr,  byte[] payload, DataOutputStream Writer) {
+		
+		super(payload);
+		this.writer = Writer;
+		this.player = plr;
+
+	}
+
+	public ClientHelloMessage decode() {
+		return this;
+	}
+
+	public void process() {
+
+		new ServerHelloMessage(this.player, this.writer).encode();
+	}
+}
